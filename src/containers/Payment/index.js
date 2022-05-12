66986bd40512fd5ded2c2812329a78538f91da12
Muhammad Waqas Rizvi <76169327+Waqas-Rizvi@users.noreply.github.com>
2022-05-12 16:07:53 +0500

    payment screen created

--- src/containers/Payment/index.js
+++ src/containers/Payment/index.js
@@ -4,11 +4,14 @@ import {
   blue_layer,
   connect,
   metamask_tag,
+  paypal_tag,
+  pink_layer,
   profile_large,
   white_layer,
 } from "../../assets";
 import { NavbarComponent } from "../../components";
 import "./css/index.css";
+import { Button } from "antd";
 
 const Payment = () => {
   return (
@@ -33,29 +36,24 @@ const Payment = () => {
         </div>
         <div style={{ border: "1px solid #2d2d2d" }}></div>
         <div
-          className="my-4 d-flex"
+          className="my-4"
           style={{
             backgroundColor: "#2b2b2b",
             width: "100%",
-            flexDirection: "column",
+            // flexDirection: "column",
             borderRadius: 20,
           }}
         >
-          <div
-            className=""
-            style={{ alignItems: "center" }}
-          >
+          <div className="d-flex paymentCardsView">
             <div
               style={{
                 backgroundColor: "#D1F5FD",
-                width: "45%",
-                height: 300,
                 borderRadius: 20,
               }}
-              className=""
+              className="paymentCards m-4"
             >
               <div className="d-flex justify-content-between">
-                <div className="m-3">
+                <div className="tapStyle">
                   <img src={metamask_tag} />
                 </div>
                 <div className="d-flex">
@@ -67,17 +65,42 @@ const Payment = () => {
                   </div>
                 </div>
               </div>
-              <span>Total Balance</span>
-              <span>$10,748</span>
-              <div>
-                <img src={connect} />
-                <span>Connect Metamask</span>
+              <div className="d-flex cardContent">
+                <h5 style={{ color: "#a4c1c7" }}>Total Balance</h5>
+                <h1 style={{ color: "#2B5963" }} className="semi-bold">
+                  $10,748
+                </h1>
+                <Button className="metamaskBtn px-4 mb-4">
+                  <img src={connect} />
+                  <span className="ms-2">Connect Metamask</span>
+                </Button>{" "}
               </div>
             </div>
-            <div
-              className="pink-gradient "
-              style={{ width: "100%", height: 300, borderRadius: 20 }}
-            ></div>
+            <div className="pink-gradient m-4 paymentCards" style={{ borderRadius: 20 }}>
+              <div className="d-flex justify-content-between">
+                <div className="tapStyle">
+                  <img src={paypal_tag} />
+                </div>
+                <div className="d-flex">
+                  <div>
+                    <img src={pink_layer} />
+                  </div>
+                  <div style={{ marginLeft: -60 }}>
+                    <img src={white_layer} />
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex cardContent">
+                <h5 style={{ color: "#c3778a" }}>Total Balance</h5>
+                <h1 style={{ color: "#2B5963" }} className="semi-bold">
+                  $10,748
+                </h1>
+                <Button className="metamaskBtn mb-4">
+                  <img src={connect} />
+                  <span className="ms-2">Connect Paypal</span>
+                </Button>{" "}
+              </div>
+            </div>
           </div>
         </div>
       </div>
