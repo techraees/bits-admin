97b2df0846d665103a2bfef3dc0ea28d5208c96a
techraees <129038132+techraees@users.noreply.github.com>
2024-07-02 17:32:22 +0500

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,7 @@
 const environment = {
+   BACKEND_BASE_URL: "https://api.bitsnft.com",
   // BACKEND_BASE_URL: "https://bits.getcoupanz.com",
   // BACKEND_BASE_URL: "https://bits-backend.onrender.com",
-  BACKEND_BASE_URL: "https://api.bitsnft.com",
   // BACKEND_BASE_URL: "http://localhost:4000",
   EMAIL_OWNER: "therealsnapboogie@gmail.com",
   INFURA: "https://ipfs.infura.io:5001/api/v0",
